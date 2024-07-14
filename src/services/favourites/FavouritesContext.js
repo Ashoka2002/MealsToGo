18630d82a7014021d29b6f7b58a197afa4d1a1e0
Ashok Choudhary <ashoka8769@gmail.com>
2024-07-14 07:47:29 +0530

    Favourites Context

---
+++ src/services/favourites/FavouritesContext.js
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+
+export const FavouritesContext = createContext();
+
+export const FavouritesContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+  const add = (restaurant) => {
+    setFavourites([...favourites, restaurant]);
+  };
+
+  const remove = (restaurant) => {
+    const newRestaurant = favourites.filter((res) => res.placeId !== restaurant.placeId);
+    setFavourites(newRestaurant);
+  };
+  return (
+    <FavouritesContext.Provider value={{ favourites, addToFavourites: add, removeFromFavourites: remove }}>
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
